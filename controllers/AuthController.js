af3279428c887724adad2f2f91f368f4af666bb9
ssatyaji <msatyaji31@gmail.com>
2022-04-17 07:15:08 +0700

    added JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,25 @@
 import user from '../models/User.js';
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import e from 'express';
+const env = dotenv.config().parsed;
+
+const generateAccessToken = async (payload) => {
+    return jsonwebtoken.sign(
+        payload,
+        env.JWT_ACCESS_TOKEN_SCRET,
+        { expiresIn: env.JWT_ACCESS_TOKEN_LIFE }
+    );
+}
+
+const generateRefreshToken = async (payload) => {
+    return jsonwebtoken.sign(
+        payload,
+        env.JWT_REFRESH_TOKEN_SCRET,
+        { expiresIn: env.JWT_REFRESH_TOKEN_LIFE }
+    );
+}
 
 const register = async (req, res) => {
     try{
@@ -12,10 +33,24 @@ const register = async (req, res) => {
             throw { code: 428, message: "Password is Required " }
         }
 
+        //check if password match
+        if(req.body.password !== req.body.retype_password) {
+            throw { code: 409, message: "PASSWORD_MUST_MATCH"}
+        }
+        
+        //check if email exist
+        const email = await user.findOne({ email: req.body.email })
+        if(email) {
+            throw { code: 428, message: "EMAIL_EXIST"}
+        }
+
+        let salt = await bcrypt.genSalt(10);
+        let hash = await bcrypt.hash(req.body.password, salt);
+
         const newUser = new user({
             fullName: req.body.fullName,
             email: req.body.email,
-            password: req.body.password,
+            password: hash,
             role: req.body.role,
         });
         const User = await newUser.save();
@@ -24,7 +59,7 @@ const register = async (req, res) => {
 
         return res.status(200).json({
             status: true,
-            message: USER_REGISTER_SUCCESS,
+            message: 'USER_REGISTER_SUCCESS',
             User
         });
     } catch(err) {
@@ -36,4 +71,71 @@ const register = async (req, res) => {
     }
 }
 
-export { register };
+const login = async (req, res) => {
+    try{
+        if(!req.body.email){
+            throw { code: 428, message: "Email is Required " }
+        }
+        if(!req.body.password){
+            throw { code: 428, message: "Password is Required " }
+        }
+        
+        //check if email exist
+        const User = await user.findOne({ email: req.body.email })
+        if(!User) { throw { code: 404, message: "EMAIL_NOT_FOUND"}}
+
+        //check is password match
+        const isMatch = await bcrypt.compareSync(req.body.password, User.password);
+        if(!isMatch) { throw { code: 428, message: "PASSWORD_WRONG" }}
+
+        //generate token
+        const payload = { id: User._id, role: User.role };
+        const accessToken = await generateAccessToken(payload);
+        const refreshToken = await generateRefreshToken(payload);
+
+        return res.status(200).json({
+            status: true,
+            message: 'LOGIN_SUCCESS',
+            accessToken,
+            refreshToken
+        });
+    } catch(err) {
+        if(!err.code) {err.code = 500}
+        return res.status(err.code).json({
+            status: false,
+            message: err.message
+        })
+    }
+}
+
+const refreshToken = async (req, res) => {
+    try{
+        if(!req.body.refreshToken){
+            throw { code: 428, message: "Refresh Token is Required " }
+        }
+
+        //verify token
+        const verify = await jsonwebtoken.verify(req.body.refreshToken, env.JWT_REFRESH_TOKEN_SCRET);
+        if(!verify) { throw { code: 401, message: "REFRESH_TOKEN_INVALID" }}
+
+        //generate token
+        let payload = { _id: verify._id, role: verify.role };
+        const accessToken = await generateAccessToken(payload);
+        const refreshToken = await generateRefreshToken(payload);
+
+        return res.status(200).json({
+            status: true,
+            message: 'REFRESH_TOKEN_SUCCESS',
+            accessToken,
+            refreshToken
+        });
+    } catch(err) {
+        if(!err.code) {err.code = 500}
+        return res.status(err.code).json({
+            status: false,
+            message: err.message
+        })
+    }
+}
+
+export { register, login, refreshToken };
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { register } from '../controllers/AuthController.js';
+import { register, login, refreshToken } from '../controllers/AuthController.js';
 var router = express.Router();
 
-router.post('/register', register);
+router.post('/register', register); // /auth/register
+router.post('/login', login); // /auth/login
+router.post('/refresh-token', refreshToken); // /auth/refreshToken
 
 export default router;
